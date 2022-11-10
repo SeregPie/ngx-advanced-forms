8411c1b65eba17991346ca8bc6cc3cc1dee3f66f
Sergej Sintschilin <seregpie@gmail.com>
2022-11-10 17:15:25 +0100

    add missing return types

--- projects/lib/src/control-hacks.ts
+++ projects/lib/src/control-hacks.ts
@@ -15,17 +15,17 @@ export const NOOP_VALUE_ACCESSOR: ControlValueAccessor = {
 export function registerControl(
 	parent: FormGroup | FormArray,
 	child: AbstractControl,
-) {
+): void {
 	child.setParent(parent);
 	(child as any)._registerOnCollectionChange(
 		(parent as any)._onCollectionChange,
 	);
 }
 
-export function unregisterControl(child: AbstractControl) {
+export function unregisterControl(child: AbstractControl): void {
 	(child as any)._registerOnCollectionChange(() => {});
 }
 
-export function triggerCollectionChange(control: AbstractControl) {
+export function triggerCollectionChange(control: AbstractControl): void {
 	(control as any)._onCollectionChange();
 }
