4a4cbb7518d96a0ec8484477552a9cba724c75ec
Sergej Sintschilin <seregpie@gmail.com>
2022-11-11 12:42:04 +0100

    add typed validator type

--- projects/lib/src/with-custom-validator.ts
+++ projects/lib/src/with-custom-validator.ts
@@ -1,17 +1,10 @@
-import {
-	AbstractControl,
-	AsyncValidatorFn,
-	ValidationErrors,
-	ValidatorFn,
-} from '@angular/forms';
-import {Observable} from 'rxjs';
+import {AbstractControl, AsyncValidatorFn, ValidatorFn} from '@angular/forms';
+
+import {TypedAsyncValidatorFn, TypedValidatorFn} from './typed-validator';
 
 export function withCustomValidator<
 	TControl extends AbstractControl = AbstractControl,
->(
-	control: TControl,
-	validator: (control: TControl) => null | ValidationErrors,
-): TControl {
+>(control: TControl, validator: TypedValidatorFn<TControl>): TControl {
 	control.addValidators(validator as ValidatorFn);
 	control.updateValueAndValidity();
 	return control;
@@ -19,12 +12,7 @@ export function withCustomValidator<
 
 export function withCustomAsyncValidator<
 	TControl extends AbstractControl = AbstractControl,
->(
-	control: TControl,
-	validator: (
-		control: TControl,
-	) => Promise<null | ValidationErrors> | Observable<null | ValidationErrors>,
-): TControl {
+>(control: TControl, validator: TypedAsyncValidatorFn<TControl>): TControl {
 	control.addAsyncValidators(validator as AsyncValidatorFn);
 	control.updateValueAndValidity();
 	return control;
