b55944fb57295540f46ad0d1acf60b2c48700276
Sergej Sintschilin <seregpie@gmail.com>
2023-02-12 15:03:26 +0100

    refactor withCustomValidator

--- src/lib/with-custom-validator.ts
+++ src/lib/with-custom-validator.ts
@@ -9,24 +9,18 @@ import {
 	CustomValidatorFn,
 } from './custom-validator';
 
-export function withCustomValidator<
-	TControl extends AbstractControl,
->(
-	control: TControl,
-	validator: CustomValidatorFn<TControl>,
-): TControl {
+export const withCustomValidator: {
+	<TControl extends AbstractControl>(control: TControl, validator: CustomValidatorFn<TControl>): TControl;
+} = (control, validator) => {
 	control.addValidators(validator as ValidatorFn);
 	control.updateValueAndValidity();
 	return control;
-}
+};
 
-export function withCustomAsyncValidator<
-	TControl extends AbstractControl,
->(
-	control: TControl,
-	validator: CustomAsyncValidatorFn<TControl>,
-): TControl {
+export const withCustomAsyncValidator: {
+	<TControl extends AbstractControl>(control: TControl, validator: CustomAsyncValidatorFn<TControl>): TControl;
+} = (control, validator) => {
 	control.addAsyncValidators(validator as AsyncValidatorFn);
 	control.updateValueAndValidity();
 	return control;
-}
+};
