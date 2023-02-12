0af1712976f07c0130e600b5367fafe97627b881
Sergej Sintschilin <seregpie@gmail.com>
2023-02-12 15:02:41 +0100

    refactor FallthroughFormService

--- src/lib/noop-control-value-accessor.ts
+++
@@ -1,8 +0,0 @@
-import {ControlValueAccessor} from '@angular/forms';
-import {noop} from 'rxjs';
-
-export const NoopControlValueAccessor: ControlValueAccessor = {
-	writeValue: noop,
-	registerOnChange: noop,
-	registerOnTouched: noop,
-};
