c9e67baf6be8a89c359641bb7dfa23e3853e04bf
Sergej Sintschilin <seregpie@gmail.com>
2023-02-12 14:58:33 +0100

    add NoopValidator

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,5 @@ export * from './custom-validator';
 export * from './dynamic-form-array';
 export * from './dynamic-form-record';
 export * from './fallthrough-form-service';
+export * from './noop-validator';
 export * from './with-custom-validator';
