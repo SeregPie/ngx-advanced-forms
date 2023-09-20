6dd57984cd6266ae167b1d5e7e602cee235c62d6
Sergej Sintschilin <seregpie@gmail.com>
2023-09-20 08:40:41 +0200

    +

--- src/bftpkesf/index.spec.ts
+++ src/bftpkesf/index.spec.ts
@@ -1,6 +1,7 @@
 import {
 	Component,
 	Injector,
+	computed,
 	effect,
 	runInInjectionContext,
 	signal,
@@ -10,7 +11,7 @@ import {TestBed, fakeAsync} from '@angular/core/testing';
 import {FormArray, FormControl, FormGroup} from '@angular/forms';
 import {formPass} from '.';
 
-describe('rbstcmre', () => {
+describe('formPass', () => {
 	it('should detect value changes', fakeAsync(() => {
 		@Component({
 			standalone: true,
@@ -21,19 +22,31 @@ describe('rbstcmre', () => {
 		let fixture = TestBed.createComponent(NoopComponent);
 
 		let s = signal<number>(1);
+		let c = computed(() => {
+			s();
+			return Math.random();
+		});
+		let aaaa = (jasmine
+			.createSpy('aaaa', () => {
+				console.log(s(), 'EFFECT')
+			})
+			.and.callThrough()
+		);
 		runInInjectionContext(injector, () => {
-			effect(() => {
-				console.log('effect', s());
-			});
+			effect(aaaa);
 		});
 
+		s.update(v => v++);
+		console.log('UPDATE');
+
 		fixture.detectChanges();
 
-		s.set(2);
+		s.update(v => v++);
+		console.log('UPDATE');
 
 		fixture.detectChanges();
 
-		console.log(s());
+		expect(aaaa).toHaveBeenCalledTimes(1);
 	}));
 
 	it('should detect status changes', fakeAsync(() => {
@@ -44,6 +57,14 @@ describe('rbstcmre', () => {
 		class NoopComponent {}
 		let injector = TestBed.inject(Injector);
 		let fixture = TestBed.createComponent(NoopComponent);
+
+		// set value
+
+		// set child value
+
+		// set validator
+
+		// set async validator
 	}));
 
 	it('should detect touched changes', fakeAsync(() => {
@@ -61,25 +82,26 @@ describe('rbstcmre', () => {
 				new FormControl<number>(0),
 			]),
 		});
+		let v0 = form.value;
 		// prettier-ignore
 		let vmvazpjf = (jasmine
 			.createSpy('vmvazpjf', () => {
-				formPass(form.controls.a).touched;
+				formPass(form).touched;
 			})
 			.and.callThrough()
 		);
 		// prettier-ignore
 		let dubrkuwp = (jasmine
 			.createSpy('dubrkuwp', () => {
-				formPass(form.controls.a).untouched;
+				formPass(form).untouched;
 			})
 			.and.callThrough()
 		);
 		// prettier-ignore
 		let mwkdxmjn = (jasmine
 			.createSpy('mwkdxmjn', () => {
-				formPass(form.controls.a).value;
-				formPass(form.controls.a).status;
+				//formPass(form).value;
+				formPass(form).status;
 			})
 			.and.callThrough()
 		);
@@ -88,17 +110,28 @@ describe('rbstcmre', () => {
 			effect(dubrkuwp);
 			effect(mwkdxmjn);
 		});
+
 		fixture.detectChanges();
+		expect(mwkdxmjn).toHaveBeenCalledTimes(1);
 		vmvazpjf.calls.reset();
 		dubrkuwp.calls.reset();
 		mwkdxmjn.calls.reset();
+
+
+
+		expect(formPass(form).touched).toBeFalse();
+		expect(formPass(form).untouched).toBeTrue();
 
 		form.markAsTouched();
 
 		expect(formPass(form).touched).toBeTrue();
 		expect(formPass(form).untouched).toBeFalse();
 
+		let v1 = form.value;
+
 		fixture.detectChanges();
+
+
 
 		expect(vmvazpjf).toHaveBeenCalledTimes(1);
 		vmvazpjf.calls.reset();
@@ -107,7 +140,7 @@ describe('rbstcmre', () => {
 		expect(mwkdxmjn).toHaveBeenCalledTimes(0);
 		mwkdxmjn.calls.reset();
 
-		form.controls.a.push(new FormControl());
+		console.log(v1 === v0);
 	}));
 
 	it('should detect pristine changes', fakeAsync(() => {
