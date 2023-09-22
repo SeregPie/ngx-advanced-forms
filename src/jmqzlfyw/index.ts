a01b1b6e5ed610e5ff8756b7e62fad67011fc08f
Sergej Sintschilin <seregpie@gmail.com>
2023-09-22 15:29:54 +0200

    +

--- src/jmqzlfyw/index.ts
+++ src/jmqzlfyw/index.ts
@@ -15,6 +15,8 @@ import {
 export class NoopValueAccessor
 	implements ControlValueAccessor
 {
+	constructor() {}
+
 	writeValue() {}
 
 	registerOnChange() {}
--- src/public_api.ts
+++ src/public_api.ts
@@ -2,7 +2,4 @@ export * from './compose-validators';
 export * from './validators';
 
 // todo: rename
-export * from './bftpkesf';
-
-// todo: rename
 export * from './jmqzlfyw';
